c60715871436946a0da7abc56a51896b10f2efbf
TrilemmaGrande <psychokater@protonmail.com>
2023-04-11 21:14:04 +0200

    TravelAnimation added

--- Map3D/Program.cs
+++ Map3D/Program.cs
@@ -4,6 +4,13 @@
     {
         static void Main(string[] args)
         {
+            Travel travel = new Travel(new Coordinate(20, 20, 20));
+            for (int i = 0; i < 100; i++)
+            {
+                Console.Clear();
+                travel.TravelAnimation();
+                Thread.Sleep(33);
+            }
 
             int mapScale = 100;
             int mapMinXY = mapScale / -2;
@@ -23,6 +30,7 @@
             }
             sector.PrintSector();
             sector.PrintStellarObjectsCoordinates();
+
         }
     }
 
@@ -131,6 +139,75 @@
                         Console.Write(" ");
                     }
                     stellarObjectPrinted = false;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+    class Travel
+    {
+        private Coordinate travelDestination;
+        private double travelTime;
+        private double travelDistance;
+
+        public Travel(Coordinate travelTarget)
+        {
+            this.travelDestination = travelTarget;
+            //this.travelDistance = Spaceship.Position -> travelTarget
+            //this.travelTime =  travelDistance / Spaceship.Speed
+            //Spaceship.Fuel -= travelDistance * Spaceship.FuelConsumption
+        }
+
+        public void TravelAnimation()
+        {
+            string[] bottomLeft = { "  ", "  ", " /", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  " };
+            string[] topLeft = { " `", "  ", " \\", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  " };
+            string[] topRight = { " ´", "  ", " /", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  " };
+            string[] bottomRight = { "  ", " \\", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  " };
+            string[] middleHorizontal = { " _", "- ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  " };
+            string[] middleVertikal = { " |", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  " };
+
+            Random rand = new Random();
+
+            for (int y = 20; y >= -20; y--)
+            {
+                for (int x = -20; x < 20; x++)
+                {
+                    if (y < -5 && x < -5)
+                    {
+                        Console.Write(bottomLeft[rand.Next(0, 15)]);
+                    }
+                    else if (y > 5 && x < -5)
+                    {
+                        Console.Write(topLeft[rand.Next(0, 15)]);
+                    }
+                    else if (y > 5 && x > 5)
+                    {
+                        Console.Write(topRight[rand.Next(0, 15)]);
+                    }
+                    else if (y < -5 && x > 5)
+                    {
+                        Console.Write(bottomRight[rand.Next(0, 14)]);
+                    }
+                    else if ((x < -5 || x > 5) && y < 5 && y > -5 && y != 0)
+                    {
+                        Console.Write(middleHorizontal[rand.Next(0, 15)]);
+                    }
+                    else if ((y < -5 || y > 5) && x > -5 && x < 5)
+                    {
+                        Console.Write(middleVertikal[rand.Next(0, 14)]);
+                    }
+                    else if (y > -5 && y < 5 && x > -5 && x < 5)
+                    {
+                        Console.Write("  ");
+                    }
+                    if (y == 0 && x == 15)
+                    {
+                        Console.Write(
+                                 $"{travelDestination.GetCoordinateX(),17}|" +
+                                 $"{travelDestination.GetCoordinateY(),3}|" +
+                                 $"{travelDestination.GetCoordinateZ(),3}");
+                    }
                 }
                 Console.WriteLine();
             }
